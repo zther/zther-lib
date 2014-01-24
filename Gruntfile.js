f0128d4821da5dc44c98549578542e7b237de6fd
Joel Caballero <joel@zther.com>
2014-01-24 11:46:21 -0800

    Adding compressed version

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,12 +17,41 @@ module.exports = function(grunt) {
       }
     },
     uglify: {
-      options: {
-        banner: '/*! <%= pkg.name.replace(".js", "") %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
-      dist: {
+      default: {
+        options: {
+          banner: '/*! <%= pkg.name.replace(".js", "") %> minified <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+        },
         files: {
           'dist/<%= pkg.name.replace(".js", "") %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      },
+      release: {
+        options: {
+          banner: '/*! <%= pkg.name.replace(".js", "") %> compressed <%= grunt.template.today("dd-mm-yyyy") %> */\n',
+          mangle: true,
+          compress: {
+            sequences     : true,  // join consecutive statemets with the “comma operator”
+            properties    : true,  // optimize property access: a["foo"] → a.foo
+            dead_code     : true,  // discard unreachable code
+            drop_debugger : true,  // discard “debugger” statements
+            unsafe        : false, // some unsafe optimizations (see below)
+            conditionals  : true,  // optimize if-s and conditional expressions
+            comparisons   : true,  // optimize comparisons
+            evaluate      : true,  // evaluate constant expressions
+            booleans      : true,  // optimize boolean expressions
+            loops         : true,  // optimize loops
+            unused        : true,  // drop unused variables/functions
+            hoist_funs    : true,  // hoist function declarations
+            hoist_vars    : false, // hoist variable declarations
+            if_return     : true,  // optimize if-s followed by return/continue
+            join_vars     : true,  // join var declarations
+            cascade       : true,  // try to cascade `right` into `left` in sequences
+            side_effects  : true,  // drop side-effect-free statements
+            warnings      : true,  // warn about potentially dangerous optimizations/code
+          }
+        },
+        files: {
+          'dist/<%= pkg.name.replace(".js", "") %>.compressed.js': ['<%= concat.dist.dest %>']
         }
       }
     },
@@ -45,7 +74,7 @@ module.exports = function(grunt) {
 
     watch: {
       files: ['<%= jshint.files %>'],
-      tasks: ['concat', 'jshint', 'qunit']
+      tasks: ['prepare', 'concat', 'jshint', 'qunit']
     }
 
   });
@@ -58,6 +87,6 @@ module.exports = function(grunt) {
   grunt.loadTasks('./tasks');
 
   grunt.registerTask('test', ['prepare','jshint', 'qunit']);
-  grunt.registerTask('default', ['prepare', 'jshint', 'concat', 'qunit', 'uglify']);
-
+  grunt.registerTask('default', ['prepare', 'jshint', 'concat', 'qunit', 'uglify:default']);
+  grunt.registerTask('release', ['prepare', 'jshint', 'concat', 'qunit', 'uglify:release']);
 };
